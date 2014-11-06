dee16ec6c4cfbd401a34d5bf7216850cdde6f28c
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-11-06 15:03:06 +0000

    adding mongo logging for backtrack recovery

--- strands_recovery_behaviours/src/strands_recovery_behaviours/recover_nav_backtrack.py
+++ strands_recovery_behaviours/src/strands_recovery_behaviours/recover_nav_backtrack.py
@@ -6,8 +6,9 @@ import actionlib
 from actionlib_msgs.msg import GoalStatus
 from backtrack_behaviour.msg import BacktrackAction, BacktrackGoal
 
+from mongo_logger import MonitoredNavEventClass
 
-#TODO add logging
+
 
 class RecoverNavBacktrack(smach.State):
     def __init__(self, max_backtrack_attempts=0, backtrack_meters_back=0.8):
@@ -16,6 +17,8 @@ class RecoverNavBacktrack(smach.State):
                              input_keys=['goal','n_nav_fails'],
                              output_keys=['goal','n_nav_fails'],
                              )
+                             
+        self.nav_stat=None
 
         rospy.set_param('max_backtrack_attempts', max_backtrack_attempts)   
         rospy.set_param('backtrack_meters_back', backtrack_meters_back)
@@ -27,11 +30,19 @@ class RecoverNavBacktrack(smach.State):
 
     def execute(self, userdata):
         
-        max_backtrack_attempts=rospy.get_param('max_backtrack_attempts',2)     
+        max_backtrack_attempts=rospy.get_param('max_backtrack_attempts',0)     
         backtrack_meters_back=rospy.get_param('backtrack_meters_back',0.8)
+        
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        
         if userdata.n_nav_fails > max_backtrack_attempts:
             return 'failure'
-        
+            
+        self.nav_stat=MonitoredNavEventClass()
+        self.nav_stat.initialize(recovery_mechanism="nav_backtrack")
+            
         backtrack_goal = BacktrackGoal()
         backtrack_goal.meters_back = backtrack_meters_back
         self.backtrack_client.send_goal(backtrack_goal)
@@ -39,9 +50,12 @@ class RecoverNavBacktrack(smach.State):
         while status == GoalStatus.PENDING or status == GoalStatus.ACTIVE:
             status = self.backtrack_client.get_state()
             if self.preempt_requested():
+                self.nav_stat.finalize(was_helped=False,n_tries=userdata.n_nav_fails)
                 self.service_preempt()
                 return 'preempted'
             self.backtrack_client.wait_for_result(rospy.Duration(0.2))
+        self.nav_stat.finalize(was_helped=False,n_tries=userdata.n_nav_fails)
+        self.nav_stat.insert()
         if status == GoalStatus.SUCCEEDED:
             return 'succeeded'
         return 'failure'
