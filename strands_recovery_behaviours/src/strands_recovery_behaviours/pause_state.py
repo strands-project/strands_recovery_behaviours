6854139247fa3034b6e4d9e8bc5d1b74320ecca2
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-11-06 15:42:20 +0000

    logging pauses - user taking over with gamepad or pause service calls - to mongodb

--- strands_recovery_behaviours/src/strands_recovery_behaviours/pause_state.py
+++ strands_recovery_behaviours/src/strands_recovery_behaviours/pause_state.py
@@ -4,14 +4,16 @@ import smach
 from monitor_pause import MonitorPause
 from monitored_navigation.recover_state_machine import RecoverStateMachine
 
-#TODO: log pauses? 
-#from mongo_logger import MonitoredNavEventClass 
+from mongo_logger import MonitoredNavEventClass 
 
 
 
 
 class PauseState(RecoverStateMachine):
     def __init__(self):
+        
+        self.nav_stat=None
+        
         RecoverStateMachine.__init__(self)
         self.resume_monitor=MonitorPause(is_paused=True)
         
@@ -21,4 +23,12 @@ class PauseState(RecoverStateMachine):
                                    transitions={'preempted':'preempted',
                                                 'invalid':'recovered_without_help',
                                                 'valid':'recovered_without_help'})
+                                                
+    def execute(self, userdata):
+        self.nav_stat=MonitoredNavEventClass()
+        self.nav_stat.initialize(recovery_mechanism="paused")
+        result=smach.StateMachine.execute(self, userdata)
+        self.nav_stat.finalize(was_helped=False,n_tries=0)
+        self.nav_stat.insert()
+        return result
  
