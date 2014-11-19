393a354568a60b6f6695bc1165c785412b852b18
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-11-19 19:19:46 +0000

    making sure bumper monitor does not try to restart motors after finding a barrier

--- strands_monitored_nav_states/src/strands_monitored_nav_states/monitor_bumper.py
+++ strands_monitored_nav_states/src/strands_monitored_nav_states/monitor_bumper.py
@@ -2,7 +2,7 @@ import rospy
 
 from monitored_navigation.monitor_state import MonitorState
 
-
+from std_msgs.msg import Bool
 from scitos_msgs.msg import MotorStatus
 
 
@@ -10,14 +10,25 @@ from scitos_msgs.msg import MotorStatus
 class MonitorBumper(MonitorState):
     def __init__(self):
         MonitorState.__init__(self, "/motor_status", MotorStatus,  self.cb)
+        
+        self.barrier_found=False
+        self.magnetic_listener = rospy.Subscriber("/magnetic_barrier", Bool,  self.barrier_cb)
+        self.magnetic_helped = rospy.Subscriber("/magnetic_barrier_helped", Bool,  self.barrier_helped_cb)
     
     """ Test the message and decide exit or not """
     def cb(self,  ud,  msg):
-        # using msg.bumper_pressed does not work properly because sometimes the
-        # bumper is pressed but no change of state is published
+        if self.barrier_found:
+            return True
+        #  msg.bumper_pressed does not  always change state when bumper is pressed
         if msg.motor_stopped and not msg.free_run:
-            #self.get_logger().log_bump()
             return False
         else:
             return True
 
+    def barrier_cb(self, msg):
+        if msg.data:
+            self.barrier_found=True
+    
+    def barrier_helped_cb(self, msg):
+        if msg.data:
+            self.barrier_found=False
