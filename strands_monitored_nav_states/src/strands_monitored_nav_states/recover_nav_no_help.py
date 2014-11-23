16268004d3e2e25c87525c11d98aaac09e35ae63
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-11-23 13:33:05 +0000

    removing clear costmaps recovery as move base seems to not be robust in terms of costmap reloading

--- strands_monitored_nav_states/src/strands_monitored_nav_states/recover_nav_no_help.py
+++ strands_monitored_nav_states/src/strands_monitored_nav_states/recover_nav_no_help.py
@@ -2,21 +2,15 @@ import rospy
 import smach
 
 from monitored_navigation.recover_state_machine import RecoverStateMachine
-from recover_nav_states import Backtrack, ClearCostmaps
+from recover_nav_states import Backtrack
 
 
 class RecoverNavNoHelp(RecoverStateMachine):
     def __init__(self):
         RecoverStateMachine.__init__(self,input_keys=['goal','n_nav_fails'],output_keys=['goal','n_nav_fails'])
         
-        self.clear_costmaps=ClearCostmaps()
         self.backtrack=Backtrack()   
         with self:
-            smach.StateMachine.add('CLEAR_COSTMAPS',
-                                   self.clear_costmaps,
-                                   transitions={'preempted':'preempted',
-                                                'try_nav':'recovered_without_help',
-                                                'do_other_recovery':'BACKTRACK'})
             smach.StateMachine.add('BACKTRACK',
                                    self.backtrack,
                                    transitions={'succeeded':'recovered_without_help',
