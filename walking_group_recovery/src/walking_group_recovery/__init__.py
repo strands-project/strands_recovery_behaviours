9f16a249345a42f41d6e87b7dfbf0ad3764dbdbc
Nils Bore <nilsbore@gmail.com>
2015-03-10 14:18:01 +0100

    Integrated the player and media server stuff

--- walking_group_recovery/src/walking_group_recovery/__init__.py
+++
@@ -1,5 +0,0 @@
-import walking_group_nav_states
-import toggle_walking_group_recovery_server
-import walking_group_recovery_state
-
-__all__ = ['walking_group_nav_states', 'toggle_walking_group_recovery_server', 'walking_group_recovery_state']
